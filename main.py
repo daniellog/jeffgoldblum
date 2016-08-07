cdafca22b262dfb04c3bd63e283ddfebf8c8d443
Daniel Townend <daniel.townend@ha247.co.uk>
2016-08-07 14:18:58 +0100

    Added username on image path

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ ugfx.area(0, 0, 320, 240, 0x0000)
 buttons.init()
 
 while not buttons.is_pressed("BTN_MENU"):
-	displayImage('apps/jeffgoldblum/jeff-goldblum.gif')
+	displayImage('apps/daniellog~jeffgoldblum/jeff-goldblum.gif')
