65013f0f6d2a9da7b2e21c5f50f7e59709039749
Daniel Townend <daniel.townend@ha247.co.uk>
2016-08-07 13:03:53 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import ugfx
+import pyb
+import buttons
+
+def displayImage(path):
+	ugfx.display_image(0,0, path)
+
+ugfx.area(0, 0, 320, 240, 0x0000)
+
+buttons.init()
+
+while not buttons.is_pressed("BTN_MENU"):
+	displayImage('apps/jeffgoldblum/jeff-goldblum.gif')
